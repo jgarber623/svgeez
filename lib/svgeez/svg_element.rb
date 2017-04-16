ed8767dd9d2f516effe64895585022a4030d702f
Jason Garber <jason@sixtwothree.org>
2017-04-16 00:36:44 -0400

    Remove build_attributes method and rename collect_source_files method.

--- lib/svgeez/svg_element.rb
+++ lib/svgeez/svg_element.rb
@@ -6,16 +6,12 @@ module Svgeez
     end
 
     def build
-      %(<svg #{build_attributes}>#{collect_source_files.join}</svg>)
+      %(<svg id="#{@destination.file_id}" version="1.1" xmlns="http://www.w3.org/2000/svg">#{source_files.join}</svg>)
     end
 
     private
 
-    def build_attributes
-      %(id="#{@destination.file_id}" version="1.1" xmlns="http://www.w3.org/2000/svg")
-    end
-
-    def collect_source_files
+    def source_files
       @source.file_paths.collect do |file_path|
         SymbolElement.new(file_path, @destination.file_id).build
       end
