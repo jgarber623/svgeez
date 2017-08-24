2052f533c8ca06e46ac71effc4a4e079c7267ae2
Jason Garber <jason@sixtwothree.org>
2017-08-24 13:47:53 -0400

    Organize commands specs

--- spec/lib/svgeez/commands/build_spec.rb
+++ spec/lib/svgeez/commands/build_spec.rb
@@ -1,15 +1,15 @@
-require 'mercenary'
-
 describe Svgeez::Commands::Build do
-  let(:program) { Mercenary::Program.new(:svgeez) }
+  describe '.init_with_program' do
+    let(:program) { Mercenary::Program.new(:svgeez) }
 
-  let(:command) { Svgeez::Commands::Build.init_with_program(program) }
+    let(:command) { Svgeez::Commands::Build.init_with_program(program) }
 
-  it 'sets a description' do
-    expect(command.description).to eq('Builds an SVG sprite from a folder of SVG icons')
-  end
+    it 'sets a description' do
+      expect(command.description).to eq('Builds an SVG sprite from a folder of SVG icons')
+    end
 
-  it 'sets a syntax' do
-    expect(command.syntax).to eq('svgeez build [options]')
+    it 'sets a syntax' do
+      expect(command.syntax).to eq('svgeez build [options]')
+    end
   end
 end
--- spec/lib/svgeez/commands/watch_spec.rb
+++ spec/lib/svgeez/commands/watch_spec.rb
@@ -1,15 +1,15 @@
-require 'mercenary'
-
 describe Svgeez::Commands::Watch do
-  let(:program) { Mercenary::Program.new(:svgeez) }
+  describe '.init_with_program' do
+    let(:program) { Mercenary::Program.new(:svgeez) }
 
-  let(:command) { Svgeez::Commands::Watch.init_with_program(program) }
+    let(:command) { Svgeez::Commands::Watch.init_with_program(program) }
 
-  it 'sets a description' do
-    expect(command.description).to eq('Watches a folder of SVG icons for changes')
-  end
+    it 'sets a description' do
+      expect(command.description).to eq('Watches a folder of SVG icons for changes')
+    end
 
-  it 'sets a syntax' do
-    expect(command.syntax).to eq('svgeez watch [options]')
+    it 'sets a syntax' do
+      expect(command.syntax).to eq('svgeez watch [options]')
+    end
   end
 end
