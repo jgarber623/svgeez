4aef6670d1b18fccb234e71fef46ac6ac29f57ae
Jason Garber <jason@sixtwothree.org>
2018-04-13 16:14:50 -0400

    Add minimum SVGO version check and disable removeViewBox SVGO plugin

--- lib/svgeez/optimizer.rb
+++ lib/svgeez/optimizer.rb
@@ -1,17 +1,24 @@
 module Svgeez
   class Optimizer
+    SVGO_MINIMUM_VERSION = '1.0.4'.freeze
+    SVGO_MINIMUM_VERSION_MESSAGE = "svgeez relies on SVGO #{SVGO_MINIMUM_VERSION} or newer. Continuing with standard sprite generation...".freeze
     SVGO_NOT_INSTALLED = 'Unable to find `svgo` in your PATH. Continuing with standard sprite generation...'.freeze
 
     def optimize(file_contents)
       return Svgeez.logger.warn(SVGO_NOT_INSTALLED) unless installed?
+      return Svgeez.logger.warn(SVGO_MINIMUM_VERSION_MESSAGE) unless supported?
 
-      `cat <<EOF | svgo --disable=cleanupIDs -i - -o -\n#{file_contents}\nEOF`
+      `cat <<EOF | svgo --disable={cleanupIDs,removeViewBox} -i - -o -\n#{file_contents}\nEOF`
     end
 
     private
 
     def installed?
       @installed ||= find_executable0('svgo')
+    end
+
+    def supported?
+      @supported ||= Gem::Version.new(`svgo -v`.strip) >= Gem::Version.new(SVGO_MINIMUM_VERSION)
     end
   end
 end
--- spec/lib/svgeez/optimizer_spec.rb
+++ spec/lib/svgeez/optimizer_spec.rb
@@ -2,10 +2,9 @@ describe Svgeez::Optimizer do
   describe '#optimize' do
     let(:logger) { Svgeez.logger }
 
-    let(:warning_message) { 'Unable to find `svgo` in your PATH. Continuing with standard sprite generation...' }
-
     context 'when SVGO executable is not found' do
       let(:optimizer) { Svgeez::Optimizer.new }
+      let(:warning_message) { 'Unable to find `svgo` in your PATH. Continuing with standard sprite generation...' }
 
       before do
         allow_any_instance_of(Svgeez::Optimizer).to receive(:installed?).and_return(nil)
@@ -18,14 +17,28 @@ describe Svgeez::Optimizer do
     end
 
     context 'when SVGO executable is found' do
-      let(:optimizer) { Svgeez::Optimizer.new }
+      context 'when SVGO version is not supported' do
+        let(:optimizer) { Svgeez::Optimizer.new }
+        let(:warning_message) { 'svgeez relies on SVGO 1.0.4 or newer. Continuing with standard sprite generation...' }
 
-      let(:input_file_contents) { IO.read('./spec/fixtures/icons/skull.svg') }
+        before do
+          allow_any_instance_of(Svgeez::Optimizer).to receive(:supported?).and_return(false)
+        end
 
-      let(:output_file_contents) { %(<svg id="skull" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="32" height="32" fill="currentcolor"><path d="M16 0C6 0 2 4 2 14v8l4 2v6h20v-6l4-2v-8C30 4 26 0 16 0M9 12a4.5 4.5 0 0 1 0 9 4.5 4.5 0 0 1 0-9m14 0a4.5 4.5 0 0 1 0 9 4.5 4.5 0 0 1 0-9"/></svg>\n) }
+        it 'logs a warning' do
+          expect(logger).to receive(:warn).with(warning_message)
+          optimizer.optimize('<svg/>')
+        end
+      end
 
-      it 'returns a string' do
-        expect(optimizer.optimize(input_file_contents)).to eq(output_file_contents)
+      context 'when SVGO version is supported' do
+        let(:optimizer) { Svgeez::Optimizer.new }
+        let(:input_file_contents) { IO.read('./spec/fixtures/icons/skull.svg') }
+        let(:output_file_contents) { %(<svg id="skull" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="32" height="32" fill="currentcolor"><path d="M16 0C6 0 2 4 2 14v8l4 2v6h20v-6l4-2v-8C30 4 26 0 16 0M9 12a4.5 4.5 0 0 1 0 9 4.5 4.5 0 0 1 0-9m14 0a4.5 4.5 0 0 1 0 9 4.5 4.5 0 0 1 0-9"/></svg>\n) }
+
+        it 'returns a string' do
+          expect(optimizer.optimize(input_file_contents)).to eq(output_file_contents)
+        end
       end
     end
   end
