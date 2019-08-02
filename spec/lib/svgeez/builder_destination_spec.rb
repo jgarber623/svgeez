1087d54d65a2d0b594d74be61fb15b0e000ca278
Jason Garber <jason@sixtwothree.org>
2019-08-02 15:36:10 -0400

    Remove low value specs

--- spec/lib/svgeez/builder_destination_spec.rb
+++
@@ -1,7 +0,0 @@
-describe Svgeez::Builder, '#destination' do
-  let(:builder) { described_class.new }
-
-  it 'returns an instance of Svgeez::Destination' do
-    expect(builder.destination).to be_instance_of(Svgeez::Destination)
-  end
-end
--- spec/lib/svgeez/builder_source_spec.rb
+++
@@ -1,7 +0,0 @@
-describe Svgeez::Builder, '#source' do
-  let(:builder) { described_class.new }
-
-  it 'returns an instance of Svgeez::Source' do
-    expect(builder.source).to be_instance_of(Svgeez::Source)
-  end
-end
