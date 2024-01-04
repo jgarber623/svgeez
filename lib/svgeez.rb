c66f1e550a4e4a0956f6a2fa46ab4223430aa2a0
Jason Garber <jason@sixtwothree.org>
2024-01-04 14:04:47 -0500

    Add rebuilt `lib/` files
    Changes include:
    
    - completely refactored Ruby interface to the gem
    - vendor-ing (thanks, MIT License) Active Support's very helpful "blank"
      extension
    
    Note that many of the deletions to the CLI commands will return later.
    This change lays the groundwork for rebuilding _those_ aspects of the
    project by first reimplementing the Ruby code in a modern, streamlined
    manner.
    
    ```ruby
    require "svgeez"
    
    Svgeez.build_sprite_sheet(
      "./app/assets/icons",
      "./app/assets/build/icons.svg",
      prefix: "custom-prefix"
    )
    ```
    
    The above example is a shortcut to an instance of
    `Svgeez::SpriteSheetBuilder` (and its `#build_sprite_sheet`) method. If
    you want or need a bit more control, you can also:
    
    ```ruby
    require "svgeez"
    
    sprite_sheet_builder = Svgeez::SpriteSheetBuilder.new(
      "./app/assets/icons",
      "./app/assets/build/icons.svg",
      prefix: "custom-prefix"
    )
    
    sprite_sheet_builder.sprite_sheet
    sprite_sheet_builder.sprites
    
    sprite_sheet_builder.build_sprite_sheet
    ```

--- lib/svgeez.rb
+++ lib/svgeez.rb
@@ -1,31 +1,19 @@
 # frozen_string_literal: true
 
-require "fileutils"
-require "logger"
-require "mkmf"
 require "securerandom"
-
-require "listen"
-require "mercenary"
 
 require_relative "svgeez/version"
 
-require_relative "svgeez/command"
-require_relative "svgeez/commands/build"
-require_relative "svgeez/commands/watch"
+require_relative "core_ext/object/blank"
 
-require_relative "svgeez/elements/svg_element"
-require_relative "svgeez/elements/symbol_element"
-
-require_relative "svgeez/builder"
-require_relative "svgeez/destination"
-require_relative "svgeez/optimizer"
-require_relative "svgeez/source"
+require_relative "svgeez/sprite"
+require_relative "svgeez/sprite_sheet"
+require_relative "svgeez/sprite_sheet_builder"
 
 module Svgeez
-  def self.logger
-    @logger ||= Logger.new($stdout)
+  # @param (see Svgeez::SpriteSheetBuilder#initialize)
+  # @return (see Svgeez::SpriteSheetBuilder#initialize)
+  def self.build_sprite_sheet(...)
+    Svgeez::SpriteSheetBuilder.new(...).build_sprite_sheet
   end
-
-  logger.formatter = proc { |_, _, _, msg| "#{msg}\n" }
 end
